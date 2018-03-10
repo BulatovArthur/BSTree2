b8a6690388ef5cf2d5bd4fe97dae9f9b179444d8
BulatovArthur <32384736+BulatovArthur@users.noreply.github.com>
2018-03-10 20:41:12 +0300

    Create bstree.cpp

---
+++ sources/bstree.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "dom.hpp"
+
+using namespace std;
+using namespace BSTree;
+
+int main(int argc, char *argv[]) {
+  Tree *tr = new Tree;
+  for (int i = 1; i < argc; i++) tr->Insert(atoi(argv[i]));
+  delete tr;
+  return 0;
+}
